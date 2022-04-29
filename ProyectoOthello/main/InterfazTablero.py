301cb80f696161f3a7330b6788112e099ba6effd
Michel Ávila <michelavila@ciencias.unam.mx>
2022-04-29 15:44:20 -0500

    menú principal en otra pantalla. Al final del juego dice quién ganó

--- ProyectoOthello/main/InterfazTablero.py
+++ ProyectoOthello/main/InterfazTablero.py
@@ -18,6 +18,7 @@ class Frame:
     def posicionEstaDentroDeEsteFrame(self, x,y):
         return x >= self.posicion[0] and y>= self.posicion[1] and x <= self.posicion[0] + self.ancho and y <= self.posicion[1] + self.alto
 
+
 """
 Clase que define un rectangulo (en el canvas) sobre el que se dibujara
 el tablero.
@@ -26,8 +27,9 @@ class TableroPanel(Frame):
     TAMANO_CASILLA_DEFAULT = 80
     CANT_CASILLAS = 8
     
-    def __init__(self, tablero):
+    def __init__(self, tablero, sidePanel):
         self.tablero = tablero
+        self.sidePanel = sidePanel
         self.posicion = (0,0)
         self.ancho = 80*8
         self.alto = 80*8
@@ -64,11 +66,20 @@ class TableroPanel(Frame):
         if tab.tablero[x+1][y+1][2] == Tablero.POSIBLE:
             #Colcamos la ficha y volteamos las fichas correspondientes
             tab.colocarFicha(x,y)
-            tab.tablero = Tree.voltearFichas(x,y, tab.tablero, tab.turno) # <------ THIS LINE IS THE PROBLEM 
+            tab.tablero = Tree.voltearFichas(x,y, tab.tablero, tab.turno)
+
+            self.seColocoUnaFicha()
+
             #Ademas de cambiar el turno
             tab.cambiarTurno()
             #Colocamos los posibles movimientos
             tab.colocaPosiblesMovimientos()
+    
+    """
+    Este se debe llamar cada que se coloque una ficha
+    """
+    def seColocoUnaFicha(self):
+        self.sidePanel.verificarFinDelJuego()
 
 """
 Clase que define la barra derecha que muestra la cuenta de fichas en el tablero,
@@ -77,22 +88,19 @@ el turno actual, y las opciones para seleccionar el nivel de dificultad.
 class SidePanel(Frame):
     ANCHO = 300
     COLOR_FONDO = (229,192,82)
-
-    BOTON_ANCHO = 120
-    BOTON_ALTO = 35
-    COLOR_BOTON =  (252, 219, 121)
-    COLOR_BOTON_SELECCIONADO = (214,175,57)
     
     def __init__(self, tablero):
         self.posicion = (TableroPanel.TAMANO_CASILLA_DEFAULT *8,0)
         self.ancho = SidePanel.ANCHO
         self.alto = TableroPanel.TAMANO_CASILLA_DEFAULT*8
         self.tablero = tablero
+        self.juegoTerminado = False
     
     def dibujar(self):
         posTexto1 = (self.posicion[0]+70,self.posicion[1]+100)
         posTexto2 = (self.posicion[0]+70,self.posicion[1]+150)
         posTexto3 = (self.posicion[0]+70,self.posicion[1]+200)
+        posTextoFinDelJuego = (self.posicion[0]+70,self.posicion[1]+450)
         posElipse = (self.posicion[0]+150,self.posicion[1]+300)
 
         fill(SidePanel.COLOR_FONDO[0], SidePanel.COLOR_FONDO[1], SidePanel.COLOR_FONDO[2])
@@ -102,8 +110,8 @@ class SidePanel(Frame):
 
         fill(0)
         textSize(16)
-        text("No. fichas negras: ____", posTexto1[0], posTexto1[1])
-        text("No. fichas blancas:____", posTexto2[0], posTexto2[1])
+        text("No. fichas negras:  " + str(self.getFichas(True)) , posTexto1[0], posTexto1[1])
+        text("No. fichas blancas: " + str(self.getFichas(False)), posTexto2[0], posTexto2[1])
     
         if(not self.tablero.turno) :
         # blancas = true, negras = false
@@ -116,78 +124,46 @@ class SidePanel(Frame):
         stroke(0)
         ellipse(posElipse[0],posElipse[1],120,120)
 
-"""
-Clase que define el boton para seleccionar la dificultad Facil.
-"""
-class BotonFacil(Frame):
-    def __init__(self, tablero):
-        self.posicion = (TableroPanel.TAMANO_CASILLA_DEFAULT *8 + 90,500)
-        self.ancho = SidePanel.BOTON_ANCHO
-        self.alto = SidePanel.BOTON_ALTO
-        self.tablero = tablero
+        if(self.juegoTerminado):
+            fill(0)
+            textSize(16)
+            text("Fin del juego. Gana: " + self.getGanador(), posTextoFinDelJuego[0], posTextoFinDelJuego[1])
     
-    def dibujar(self):
-        fill(SidePanel.COLOR_BOTON[0], SidePanel.COLOR_BOTON[1], SidePanel.COLOR_BOTON[2])
-        if self.tablero.dificultad == Tablero.DIFICULTAD_FACIL:
-            fill(SidePanel.COLOR_BOTON_SELECCIONADO[0], SidePanel.COLOR_BOTON_SELECCIONADO[1], SidePanel.COLOR_BOTON_SELECCIONADO[2])
-        stroke(0)
-        strokeWeight(1.2)
-        rect(self.posicion[0],self.posicion[1],self.ancho,self.alto)
-
-        fill(0)
-        textSize(16)
-        text("Facil", self.posicion[0]+10, self.posicion[1]+20)
-
-    def onMousePressed(self, mouseX, mouseY):
-        print("dificultad facil")
-
-"""
-Clase que define el boton para seleccionar la dificultad Facil.
-"""
-class BotonMedia(Frame):
-    def __init__(self, tablero):
-        self.posicion = (TableroPanel.TAMANO_CASILLA_DEFAULT *8 + 90, 500 + SidePanel.BOTON_ALTO + 5)
-        self.ancho = SidePanel.BOTON_ANCHO
-        self.alto = SidePanel.BOTON_ALTO
-        self.tablero = tablero
+    def getFichas(self, color):
+        """
+        color == true, negro
+        color == false, blanco
+        """
+        tablero = self.tablero.tablero
+        fichas = 0
+        for i in range(8):
+            for j in range(8):
+                if color:
+                    if(tablero[j+1][i+1][2]==Tablero.NEGRO):
+                        fichas += 1
+                else:
+                    if(tablero[j+1][i+1][2]==Tablero.BLANCO):
+                        fichas += 1
+        return fichas
     
-    def dibujar(self):
-        fill(SidePanel.COLOR_BOTON[0], SidePanel.COLOR_BOTON[1], SidePanel.COLOR_BOTON[2])
-        if self.tablero.dificultad == Tablero.DIFICULTAD_MEDIA:
-            fill(SidePanel.COLOR_BOTON_SELECCIONADO[0], SidePanel.COLOR_BOTON_SELECCIONADO[1], SidePanel.COLOR_BOTON_SELECCIONADO[2])
-        stroke(0)
-        strokeWeight(1.2)
-        rect(self.posicion[0],self.posicion[1],self.ancho,self.alto)
-
-        fill(0)
-        textSize(16)
-        text("Intermedio", self.posicion[0]+10, self.posicion[1]+20)
+    """
+    Cambia la variable juegoTerminado a True si ninguno de los jugadores puede tirar en el tablero en su estado actual.
+    """
+    def verificarFinDelJuego(self):
+        tablero = self.tablero.tablero
+        posiblesMovimientosJugador1 = Tree.generaPosiblesMovimiento(tablero, True)
+        posiblesMovimientosJugador2 = Tree.generaPosiblesMovimiento(tablero, False)
+        self.juegoTerminado = (not posiblesMovimientosJugador1) and (not posiblesMovimientosJugador2)
     
-    def onMousePressed(self, mouseX, mouseY):
-        print("dificultad media")
-
-"""
-Clase que define el boton para seleccionar la dificultad Facil.
-"""
-class BotonDificil(Frame):
-    def __init__(self, tablero):
-        self.posicion = (TableroPanel.TAMANO_CASILLA_DEFAULT *8 + 90, 500 + SidePanel.BOTON_ALTO*2 + 10)
-        self.ancho = SidePanel.BOTON_ANCHO
-        self.alto = SidePanel.BOTON_ALTO
-        self.tablero = tablero
-    
-    def dibujar(self):
-        fill(SidePanel.COLOR_BOTON[0], SidePanel.COLOR_BOTON[1], SidePanel.COLOR_BOTON[2])
-        if self.tablero.dificultad == Tablero.DIFICULTAD_DIFICIL:
-            fill(SidePanel.COLOR_BOTON_SELECCIONADO[0], SidePanel.COLOR_BOTON_SELECCIONADO[1], SidePanel.COLOR_BOTON_SELECCIONADO[2])
-        stroke(0)
-        strokeWeight(1.2)
-        rect(self.posicion[0],self.posicion[1],self.ancho,self.alto)
-
-        fill(0)
-        textSize(16)
-        text("Dificil", self.posicion[0]+10, self.posicion[1]+20)
-
-    def onMousePressed(self, mouseX, mouseY):
-        print("dificil")
-    
+    """
+    Si el juego se acabara en este momento, este metodo regresa que fichas ganaron (Negras, Blancas o Empate)
+    """
+    def getGanador(self):
+        totalNegras = self.getFichas(True)
+        totalBlancas = self.getFichas(False)
+        if totalNegras > totalBlancas :
+            return "Negras"
+        elif totalNegras < totalBlancas :
+            return "Blancas"
+        else:
+            return "Empate"
--- ProyectoOthello/main/main.pyde
+++ ProyectoOthello/main/main.pyde
@@ -1,18 +1,54 @@
 from Tablero import Tablero
 from Tree import Tree
-from InterfazTablero import TableroPanel, SidePanel, BotonFacil, BotonMedia, BotonDificil
+from InterfazTablero import TableroPanel, SidePanel, Frame
 
 '''
 Archivo encargado de mostrar la interfaz grafica
 '''
 
-tab=Tablero()
+# Dimensiones:
+ANCHO_VENTANA = 300 + 8*80
+ALTO_VENTANA = 8*80
+BOTON_ANCHO = 120
+BOTON_ALTO = 35
+# Colores:
+COLOR_FONDO_MENU = (229,192,82)
+COLOR_BOTON = (252, 219, 121)
+COLOR_BOTON_SELECCIONADO = (214,175,57)
 
-interfazTab = TableroPanel(tab)
-sidePanel = SidePanel(tab)
-botonFacil = BotonFacil(tab)
-botonMedia = BotonMedia(tab)
-botonDificil = BotonDificil(tab)
+
+"""
+Clase que define un boton.
+"""
+class Boton(Frame):
+    def __init__(self, posicion, texto):
+        self.posicion = posicion
+        self.ancho = BOTON_ANCHO
+        self.alto = BOTON_ALTO
+        self.texto = texto
+        self.color_fondo = COLOR_BOTON
+        self.color_selected = COLOR_BOTON_SELECCIONADO
+
+    def dibujar(self):
+        fill(self.color_fondo[0], self.color_fondo[1], self.color_fondo[2])
+        stroke(0)
+        strokeWeight(1.2)
+        rect(self.posicion[0],self.posicion[1],self.ancho,self.alto)
+
+        fill(0)
+        textSize(16)
+        text(self.texto, self.posicion[0]+10, self.posicion[1]+20)
+
+
+# -----------------------------------------------------------
+
+# Inicializo los componentes de la interfaz:
+botonFacil = Boton((400,300), "Facil")
+botonMedia = Boton((400,300 + BOTON_ALTO + 5), "Media")
+botonDificil = Boton((400,300 + 2*(BOTON_ALTO + 5)), "Dificil")
+botonMenuPrincipal = Boton((80*8+90,500), "Volver al menu")
+
+mostrar_menu = True
 
 
 #Creamos la ventana
@@ -22,24 +58,57 @@ def setup():
 
 #Pintamos el tablero de acuerdo a la variable tablero que es una matriz de 10x10x3
 def draw():
-    interfazTab.dibujar()
-    sidePanel.dibujar()
-    botonFacil.dibujar()
-    botonMedia.dibujar()
-    botonDificil.dibujar()
+    if mostrar_menu :
+        dibujarMenu()
+    else:
+        tableroPanel.dibujar()
+        sidePanel.dibujar()
+        botonMenuPrincipal.dibujar()
 
 #Capturamos un click en la ventana
 def mousePressed():
+
+    if mostrar_menu :
+        if botonFacil.posicionEstaDentroDeEsteFrame(mouseX, mouseY):
+            iniciarJuego("facil")
     
-    if interfazTab.posicionEstaDentroDeEsteFrame(mouseX, mouseY):
-        interfazTab.onMousePressed(mouseX, mouseY)
+        if botonMedia.posicionEstaDentroDeEsteFrame(mouseX, mouseY):
+            iniciarJuego("medio")
     
-    if botonFacil.posicionEstaDentroDeEsteFrame(mouseX, mouseY):
-        botonFacil.onMousePressed(mouseX, mouseY)
+        if botonDificil.posicionEstaDentroDeEsteFrame(mouseX, mouseY):
+            iniciarJuego("dificil")
+    else:
+        if tableroPanel.posicionEstaDentroDeEsteFrame(mouseX, mouseY):
+            tableroPanel.onMousePressed(mouseX, mouseY)
+        
+        if botonMenuPrincipal.posicionEstaDentroDeEsteFrame(mouseX, mouseY):
+            global mostrar_menu
+            mostrar_menu = True
+
+
+def iniciarJuego(dificultad):
+    global mostrar_menu
+    mostrar_menu = False
+
+    tab = Tablero() # tab = Tablero(dificultad)
+    global sidePanel
+    sidePanel = SidePanel(tab)
+    global tableroPanel
+    tableroPanel = TableroPanel(tab, sidePanel)
+
+
+def dibujarMenu():
+    posTexto1 = (380,280)
+
+    fill(COLOR_FONDO_MENU[0], COLOR_FONDO_MENU[1], COLOR_FONDO_MENU[2])
+    stroke(0)
+    strokeWeight(1.2)
+    rect(0,0,ANCHO_VENTANA,ALTO_VENTANA)
+
+    fill(0)
+    textSize(16)
+    text("Elige un nivel de dificultad:", posTexto1[0], posTexto1[1])
     
-    if botonMedia.posicionEstaDentroDeEsteFrame(mouseX, mouseY):
-        botonMedia.onMousePressed(mouseX, mouseY)
-    
-    if botonDificil.posicionEstaDentroDeEsteFrame(mouseX, mouseY):
-        botonDificil.onMousePressed(mouseX, mouseY)
-    
+    botonFacil.dibujar()
+    botonMedia.dibujar()
+    botonDificil.dibujar()
